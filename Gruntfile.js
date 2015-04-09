a97a733c4064f4523d7726e12f75eb1a20048e0b
Ara Yeressian <ayeressian@macadamian.com>
2015-04-09 21:41:50 +0400

    partial target size change fix, grunt git integration phase 1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,13 @@
 /*global module:false */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     'use strict';
-    
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jsbeautifier: {
-            files: ['**/*.js', '**/*.css' , '**/*.html', '!bower_components/**/*', '!node_modules/**/*'],
+            files: ['**/*.js', '**/*.css', '**/*.html', '!bower_components/**/*', '!node_modules/**/*'],
             options: {}
         },
         jshint: {
@@ -15,9 +15,20 @@ module.exports = function(grunt) {
             options: {
                 jshintrc: true
             }
+        },
+        githooks: {
+            all: {
+                options: {                    
+                },
+                'pre-commit': 'jshint',
+                'post-merge': {
+                    taskNames: 'bower:install'
+                }
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-githooks');
     grunt.loadNpmTasks('grunt-jsbeautifier');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 

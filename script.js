2bee47ab524114cedf49cce0ec9850debcd093e5
Ara Yeressian <ayeressian@macadamian.com>
2015-05-03 17:58:49 +0400

    deleted local test

--- script.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('#scrollbar-vert').scrollbar($('#flexible-scrollbar-target-vert'));
-    $('#scrollbar-horiz').scrollbar($('#flexible-scrollbar-target-horiz'), true);
-    $('#increase').on('click', function() {
-        $('.hidden').removeClass('hidden');
-    });
-});

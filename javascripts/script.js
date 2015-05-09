fc88ebaedfdc21a4618948d8c07ad79587898a45
Ara Yeressian <ayeressian@macadamian.com>
2015-05-09 19:47:50 +0400

    Added "not next to example" Added buttons for modifying the container properties Added example code

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,31 @@
 $(function() {
     $('#scrollbar-vert').scrollbar($('#flexible-scrollbar-target-vert'));
     $('#scrollbar-horiz').scrollbar($('#flexible-scrollbar-target-horiz'), true);
+    $('#scrollbar-vert-distant').scrollbar($('#flexible-scrollbar-target-vert-distant'));
+    $('#increase-inner').on('click', function() {
+        $('.red').removeClass('hidden');
+    });
+    $('#decrease-inner').on('click', function() {
+        $('.red').addClass('hidden');
+    });
     $('#increase').on('click', function() {
-        $('.hidden').removeClass('hidden');
+        $('#flexible-scrollbar-target-vert').addClass('big');
+        $('#flexible-scrollbar-target-horiz').addClass('big');
+
+        $('#scrollbar-vert').addClass('big');
+        $('#scrollbar-horiz').addClass('big');
+
+        $('#default-browser-vert').addClass('big');
+        $('#default-browser-horiz').addClass('big');
+    });
+    $('#decrease').on('click', function() {
+        $('#flexible-scrollbar-target-vert').removeClass('big');
+        $('#flexible-scrollbar-target-horiz').removeClass('big');
+
+        $('#scrollbar-vert').removeClass('big');
+        $('#scrollbar-horiz').removeClass('big');
+
+        $('#default-browser-vert').removeClass('big');
+        $('#default-browser-horiz').removeClass('big');
     });
 });

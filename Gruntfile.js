265e48e4c958996d078cdf26aa891a7ed7971eb8
Ara Yeressian <ayeressian@macadamian.com>
2015-07-03 17:27:12 +0400

    added cssmin, fixed default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,58 @@
 /*global module:false */
 
 module.exports = function(grunt) {
-  'use strict';
+    'use strict';
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jsbeautifier: {
-      files: ['**/*.js', '**/*.css', '**/*.html', '!bower_components/**/*', '!node_modules/**/*'],
-      options: {}
-    },
-    jshint: {
-      all: ['**/*.js', '!bower_components/**/*', '!node_modules/**/*'],
-      options: {
-        jshintrc: true
-      }
-    },
-    githooks: {
-      all: {
-        options: {},
-        'pre-commit': 'jshint jsbeautifier',
-        'post-merge': {
-          taskNames: 'bower:install'
-        }
-      }
-    },
-    uglify: {
-      my_target: {
-        options: {
-          sourceMap: true
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jsbeautifier: {
+            files: ['**/*.js', '**/*.css', '**/*.html', '!**/*.min.*', '!bower_components/**/*', '!node_modules/**/*'],
+            options: {}
         },
-        files: {
-          'flexible-scrollbar/flexible-scrollbar.min.js': ['flexible-scrollbar/flexible-scrollbar.js']
+        jshint: {
+            all: ['**/*.js', '!**/*.min.js', '!bower_components/**/*', '!node_modules/**/*'],
+            options: {
+                jshintrc: true
+            }
+        },
+        githooks: {
+            all: {
+                options: {},
+                'pre-commit': 'jshint jsbeautifier',
+                'post-merge': {
+                    taskNames: 'bower:install'
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'flexible-scrollbar/flexible-scrollbar.min.js': ['flexible-scrollbar/flexible-scrollbar.js']
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'flexible-scrollbar/flexible-scrollbar.min.css': ['flexible-scrollbar/flexible-scrollbar.min.css']
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-githooks');
-  grunt.loadNpmTasks('grunt-jsbeautifier');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-githooks');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['jshint', 'jsbeautifier']);
-  grunt.registerTask('pre-commit', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'jsbeautifier', 'uglify', 'cssmin']);
+    grunt.registerTask('pre-commit', ['jshint']);
 };

9f9ec5a8706f76a281b09c3354e4b0a64b03701b
Ara Yeressian <ayeressian2@gmail.com>
2016-01-09 21:06:47 +0400

    added comments on functions

--- flexible-scrollbar/flexible-scrollbar.js
+++ flexible-scrollbar/flexible-scrollbar.js
@@ -55,7 +55,7 @@
          * @return {boolean}
          */
         isSafari: function() {
-            return navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+            return navigator.userAgent.indexOf('Safari') !== -1 && navigator.userAgent.indexOf('Chrome') === -1;
         }
     };
     /*Util end*/
@@ -98,7 +98,8 @@
      * Determines the amount of scrollbar movement on each scrollbar arrow click.
      * @constant
      * @type {Number}
-     * @default
+     * @static
+     * @private
      */
     Scrollbar._SLIDER_ARROW_AMOUNT = 15;
 
@@ -107,7 +108,8 @@
      * this is the render frame interval of scrollbar arrow click and scrollbar thumb movement.
      * @constant
      * @type {Number}
-     * @default
+     * @static
+     * @private
      */
     Scrollbar._CONST_MOVE_MIL = 75;
 
@@ -117,15 +119,25 @@
      * variable.
      * @constant
      * @type {Number}
-     * @default
+     * @static
+     * @private
      */
     Scrollbar._MINIMUM_SLIDER_SIZE = 40;
 
+    /**
+     * Sets the slider (thumb) position to current position insider thumb bed
+     * @private
+     */
     Scrollbar.prototype._setSliderPosFromTarget = function() {
         var targetSliderCalculatedPos = this._scroll(this._$targetElement) / (this._scrollSize(this._$targetElement) - this._size(this._$targetElement));
         this._currentSliderPos = (this._sliderBedSize - this._sliderSize) * targetSliderCalculatedPos;
         this._position(this._$slider, this._currentSliderPos);
     };
+
+    /**
+     * Update slider (thumb) size based on current container and content dimensions
+     * @private
+     */
     Scrollbar.prototype._updateSliderSize = function() {
         var scrollSize, targetElementRatio;
         this._sliderBedSize = this._size(this._$sliderBed);
@@ -144,11 +156,22 @@
         this._setSliderPosFromTarget();
     };
 
+    /**
+     * Ends scrolling for arrow click
+     * @private
+     */
     Scrollbar.prototype._disableScrolling = function() {
         this._util.enableSelection();
         this._mousePosRelativeToSlider = null;
         this._stopArrowMouseDown = true;
     };
+
+    /**
+     * Sets new position for slider (thumb).
+     * @private
+     * @param  {number} sliderPos New slider position.
+     * @return {boolean}          Determines if thumb (slider) reached to end or beginning of slider bed.
+     */
     Scrollbar.prototype._setSliderPos = function(sliderPos) {
         var edgeReached = false,
             sliderCalculatedPos;
@@ -169,15 +192,33 @@
         this._currentSliderPos = sliderPos;
         return edgeReached;
     };
+
+    /**
+     * Decreases current slider position.
+     * @private
+     * @return {boolean} If the slider reached to the end or beginning of slider bed.
+     */
     Scrollbar.prototype._decreaseArrow = function() {
         var newSliderPos = this._positionRelParent(this._$slider) - Scrollbar._SLIDER_ARROW_AMOUNT;
         return this._setSliderPos(newSliderPos);
     };
+
+    /**
+     * Increases current slider position.
+     * @private
+     * @return {boolean} If the slider reached to the end or beginning of slider bed.
+     */
     Scrollbar.prototype._increaseArrow = function() {
         var newSliderPos = this._positionRelParent(this._$slider) + Scrollbar._SLIDER_ARROW_AMOUNT;
         return this._setSliderPos(newSliderPos);
     };
-    //TODO: Change to css animation for better performance.
+
+    /**
+     * Animates the speed decreasing movement of scroll.
+     * @todo  Change to css animation for better performance.
+     * @param  {number} velocity Initial animation speed.
+     * @private
+     */
     Scrollbar.prototype._touchInertiaAnimator = function(velocity) {
         var MILLISECOND_PER_FRAME = 16,
             FRICTION_COEFFICIENT = 0.95,
@@ -205,6 +246,11 @@
             self._setSliderPosFromTarget();
         }, MILLISECOND_PER_FRAME, this);
     };
+
+    /**
+     * Initialises all the touch events.
+     * @private
+     */
     Scrollbar.prototype._initTouchEvents = function() {
         var touchPos, eventForSwipeLatest, eventForSwipeOneBeforeLatest;
 
@@ -246,6 +292,11 @@
             touchPos = undefined;
         }, this));
     };
+
+    /**
+     * Initialises mouse wheel or trackpad scrolling events.
+     * @private
+     */
     Scrollbar.prototype._initMouseWheel = function() {
         if (this._$targetElement.mousewheel) {
             this._$targetElement.on('mousewheel', $.proxy(function(event) {
@@ -263,6 +314,11 @@
             }, this));
         }
     };
+
+    /**
+     * Initialises all events.
+     * @private
+     */
     Scrollbar.prototype._initEvents = function() {
         //fetch the slider pos for resizing
         this._$slider.on('mousedown touchstart', $.proxy(function(event) {
@@ -316,9 +372,130 @@
             }, Scrollbar._CONST_MOVE_MIL, this);
         }, this));
     };
+
+    /**
+     * Gets or sets $element size
+     * @abstract
+     * @param  {object} $element A Jquery object.
+     * @param  {number} size     New size of $element.
+     * @return {number} size     Size of $element. Will be undefined if second
+     *                           param value exist.
+     */
+    Scrollbar.prototype._size = function($element, size) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * Returns scroll size. Or in other words real size of $element.
+     * @abstract
+     * @param  {Object} $element A Jquery object.
+     * @return {number}          Real size of $element, with considering hidden region,
+     *                           which is covered by scroll control.
+     */
+    Scrollbar.prototype._scrollSize = function($element) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * Sets or gets position of $element
+     * @abstract
+     * @param  {Object} $element A Jquery object.
+     * @param  {number} position New position of $element.
+     * @return {number}          Position of $element. Will be undefined if second
+     *                           param value exist.
+     */
+    Scrollbar.prototype._position = function($element, position) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * Get touch coordinate
+     * @abstract
+     * @param  {Object} $event A Jquery event object.
+     * @return {number}        Touch coordinate.
+     */
+    Scrollbar.prototype._touchSinglePagePos = function($event) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * Gets offset event.
+     * @abstract
+     * @param  {Object} $event A Jquery event Object.
+     * @return {Object}        A Jquery event Object.
+     */
+    Scrollbar.prototype._offsetEvent = function($event) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * Gets $element offset.
+     * @abstract
+     * @param  {Object} $element A Jquery object.
+     * @return {number}          Offset amount.
+     */
+    Scrollbar.prototype._offsetElement = function($element) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * [function description]
+     * @abstract
+     * @param  {[type]} $event [description]
+     * @return {[type]}        [description]
+     */
+    Scrollbar.prototype._page = function($event) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * Move the container scroll to designated position.
+     * @abstract
+     * @param  {Object} $element A Jquery Object.
+     * @param  {number} position Position of target scrollbar.
+     * @return {number}          Position of scroll if position param hasn't
+     *                           been specified.
+     */
+    Scrollbar.prototype._scroll = function($element, position) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * Position of $element relative to parent.
+     * @abstract
+     * @param  {Object} $element A Jquery Object.
+     */
+    Scrollbar.prototype._positionRelParent = function($element) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * Gets delta property of mouse wheel event.
+     * @abstract
+     * @param  {Object} $event A Jquery event.
+     * @return {Number}        Delta value of mouse wheel event.
+     */
+    Scrollbar.prototype._mousewheelDelta = function($event) {
+        throw new Error('Must be implemented by subclass.');
+    };
+
+    /**
+     * Workaround bug in Jquery mousewheel. Gets mouse wheel orientation.
+     * @abstract
+     * @return {number} Orientation
+     */
+    Scrollbar.prototype._mousewheelOrientation = function() {
+        throw new Error('Must be implemented by subclass.');
+    };
     /*Scrollbar end*/
 
     /*ScrollbarHorizontal*/
+    /**
+     * This class represents horizontal scrollbar.
+     * @constructor
+     * @augments Scrollbar
+     * @see super
+     */
     function ScrollbarHorizontal($scrollbar, $targetElement, util, minSliderSize) {
         var html = '<div class="scrollbar horizontal">' +
             '<div class="left-arrow arrow"></div>' +
@@ -332,26 +509,45 @@
         Scrollbar.call(this, $scrollbar, $targetElement, util, minSliderSize);
     }
     ScrollbarHorizontal.prototype = Object.create(Scrollbar.prototype);
+
+    /**
+     * @see super._size
+     */
     ScrollbarHorizontal.prototype._size = function($element, size) {
         if (size) {
             return $element.width(size);
         }
         return $element.width();
     };
+
+    /**
+     * @see super._scrollSize
+     */
     ScrollbarHorizontal.prototype._scrollSize = function($element) {
         return $element.prop('scrollWidth');
     };
+
+    /**
+     * @see super._position
+     */
     ScrollbarHorizontal.prototype._position = function($element, position) {
         if (position !== null && position !== undefined) {
             return $element.css('left', position);
         }
         return $element.css('left');
     };
+
+    /**
+     * @see super._touchSinglePagePos
+     */
     ScrollbarHorizontal.prototype._touchSinglePagePos = function($event) {
-        var touch;
-        touch = $event.originalEvent.touches[0] || $event.originalEvent.changedTouches[0];
+        var touch = $event.originalEvent.touches[0] || $event.originalEvent.changedTouches[0];
         return touch.pageX;
     };
+
+    /**
+     * @see super._offsetEvent
+     */
     ScrollbarHorizontal.prototype._offsetEvent = function($event) {
         var touch, offsetX;
         if (this._util.isTouchEvent($event)) {
@@ -361,9 +557,17 @@
         }
         return $event.offsetX || $event.pageX - $($event.target).offset().left;
     };
+
+    /**
+     * @see super._offsetElement
+     */
     ScrollbarHorizontal.prototype._offsetElement = function($element) {
         return $element.offset().left;
     };
+
+    /**
+     * @see super._page
+     */
     ScrollbarHorizontal.prototype._page = function($event) {
         if (this._util.isTouchEvent($event)) {
             var touch = $event.originalEvent.touches[0] || $event.originalEvent.changedTouches[0];
@@ -371,25 +575,46 @@
         }
         return $event.pageX;
     };
+
+    /**
+     * @see super._scroll
+     */
     ScrollbarHorizontal.prototype._scroll = function($element, amount) {
         if (amount !== null && amount !== undefined) {
             return $element.scrollLeft(amount);
         }
         return $element.scrollLeft();
     };
+
+    /**
+     * @see super._positionRelParent
+     */
     ScrollbarHorizontal.prototype._positionRelParent = function($element) {
         return $element.position().left;
     };
+
+    /**
+     * @see super._mousewheelDelta
+     */
     ScrollbarHorizontal.prototype._mousewheelDelta = function($event) {
         return $event.deltaX;
     };
-    //workaround bug in jquery mousewheel
+
+    /**
+     * @see super._mousewheelOrientation
+     */
     ScrollbarHorizontal.prototype._mousewheelOrientation = function() {
         return 1;
     };
     /*ScrollbarHorizontal end*/
 
     /*ScrollbarVertical*/
+    /**
+     * This class represents vertical scrollbar.
+     * @constructor
+     * @augments Scrollbar
+     * @see super
+     */
     function ScrollbarVertical($scrollbar, $targetElement, util, minSliderSize) {
         var html = '<div class="scrollbar vertical">' +
             '<div class="top-arrow arrow"></div>' +
@@ -403,26 +628,45 @@
         Scrollbar.call(this, $scrollbar, $targetElement, util, minSliderSize);
     }
     ScrollbarVertical.prototype = Object.create(Scrollbar.prototype);
+
+    /**
+     * @see super._size
+     */
     ScrollbarVertical.prototype._size = function($element, size) {
         if (size) {
             return $element.height(size);
         }
         return $element.height();
     };
+
+    /**
+     * @see super._scrollSize
+     */
     ScrollbarVertical.prototype._scrollSize = function($element) {
         return $element.prop('scrollHeight');
     };
+
+    /**
+     * @see super._position
+     */
     ScrollbarVertical.prototype._position = function($element, position) {
         if (position !== null && position !== undefined) {
             return $element.css('top', position);
         }
         return $element.css('top');
     };
+
+    /**
+     * @see super._touchSinglePagePos
+     */
     ScrollbarVertical.prototype._touchSinglePagePos = function($event) {
-        var touch;
-        touch = $event.originalEvent.touches[0] || $event.originalEvent.changedTouches[0];
+        var touch = $event.originalEvent.touches[0] || $event.originalEvent.changedTouches[0];
         return touch.pageY;
     };
+
+    /**
+     * @see super._offsetEvent
+     */
     ScrollbarVertical.prototype._offsetEvent = function($event) {
         var touch, offsetY;
         if (this._util.isTouchEvent($event)) {
@@ -432,9 +676,17 @@
         }
         return $event.offsetY || $event.pageY - $($event.target).offset().top;
     };
+
+    /**
+     * @see super._offsetElement
+     */
     ScrollbarVertical.prototype._offsetElement = function($element) {
         return $element.offset().top;
     };
+
+    /**
+     * @see super._page
+     */
     ScrollbarVertical.prototype._page = function($event) {
         if (this._util.isTouchEvent($event)) {
             var touch = $event.originalEvent.touches[0] || $event.originalEvent.changedTouches[0];
@@ -442,19 +694,34 @@
         }
         return $event.pageY;
     };
+
+    /**
+     * @see super._scroll
+     */
     ScrollbarVertical.prototype._scroll = function($element, amount) {
         if (amount !== null && amount !== undefined) {
             return $element.scrollTop(amount);
         }
         return $element.scrollTop();
     };
+
+    /**
+     * @see super._positionRelParent
+     */
     ScrollbarVertical.prototype._positionRelParent = function($element) {
         return $element.position().top;
     };
+
+    /**
+     * @see super._mousewheelDelta
+     */
     ScrollbarVertical.prototype._mousewheelDelta = function($event) {
         return $event.deltaY;
     };
-    //workaround bug in jquery mousewheel
+
+    /**
+     * @see super._mousewheelOrientation
+     */
     ScrollbarVertical.prototype._mousewheelOrientation = function() {
         return -1;
     };
